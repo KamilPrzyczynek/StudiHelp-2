b8c62e753154b8e1cd2c08f808c6d5bb74de07aa
TheDarkness016 <117547153+TheDarkness016@users.noreply.github.com>
2024-05-25 02:22:43 +0200

    Update EditProfilActivity.kt

--- app/src/main/java/com/example/studihelp/ui/login/EditProfilActivity.kt
+++ app/src/main/java/com/example/studihelp/ui/login/EditProfilActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studihelp.MainActivity2
 import com.example.studihelp.R
@@ -16,92 +15,91 @@ import com.google.firebase.database.FirebaseDatabase
 
 class EditProfilActivity : AppCompatActivity() {
 
-
-    var editName: EditText? = null
-    var editEmail:EditText? = null
-    var editUsername:EditText? = null
-    var editPassword:EditText? = null
-    var saveButton: Button? = null
-    var nameUser: String? = null
-    var emailUser:String? = null
-    var usernameUser:String? = null
-    var passwordUser:String? = null
-    var reference: DatabaseReference? = null
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editUsername: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var saveButton: Button
     private lateinit var backtohomeText: TextView
+    private lateinit var reference: DatabaseReference
 
+    private var nameUser: String? = null
+    private var emailUser: String? = null
+    private var usernameUser: String? = null
+    private var passwordUser: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_edit_profil)
+
         reference = FirebaseDatabase.getInstance().getReference("users")
-        editName = findViewById<EditText>(R.id.editName)
-        editEmail = findViewById<EditText>(R.id.editEmail)
-        editUsername = findViewById<EditText>(R.id.editUsername)
-        editPassword = findViewById<EditText>(R.id.editPassword)
-        saveButton = findViewById<Button>(R.id.saveButton)
+        editName = findViewById(R.id.editName)
+        editEmail = findViewById(R.id.editEmail)
+        editUsername = findViewById(R.id.editUsername)
+        editPassword = findViewById(R.id.editPassword)
+        saveButton = findViewById(R.id.saveButton)
         backtohomeText = findViewById(R.id.backtohomeText)
 
-
         showData()
+
         backtohomeText.setOnClickListener {
             val intent = Intent(this@EditProfilActivity, MainActivity2::class.java)
             startActivity(intent)
         }
 
-        saveButton?.setOnClickListener(View.OnClickListener {
+        saveButton.setOnClickListener {
             if (isNameChanged() || isEmailChanged() || isPasswordChanged()) {
                 Toast.makeText(this@EditProfilActivity, "Saved", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this@EditProfilActivity, "No Changes Found", Toast.LENGTH_SHORT)
                     .show()
             }
-        })
+        }
     }
 
-    fun isNameChanged(): Boolean {
-        return if (editName != null && nameUser != editName!!.text.toString()) {
-            reference!!.child(usernameUser!!).child("name")
-                .setValue(editName!!.text.toString())
-            nameUser = editName!!.text.toString()
+    private fun isNameChanged(): Boolean {
+        return if (nameUser != editName.text.toString()) {
+            reference.child(usernameUser!!).child("name")
+                .setValue(editName.text.toString())
+            nameUser = editName.text.toString()
             true
         } else {
             false
         }
     }
 
-
-    fun isEmailChanged(): Boolean {
-        return if (emailUser != editName!!.getText().toString()) {
-            reference!!.child(usernameUser!!).child("email")
-                .setValue(editEmail!!.getText().toString())
-            emailUser = editEmail!!.getText().toString()
+    private fun isEmailChanged(): Boolean {
+        return if (emailUser != editEmail.text.toString()) {
+            reference.child(usernameUser!!).child("email")
+                .setValue(editEmail.text.toString())
+            emailUser = editEmail.text.toString()
             true
         } else {
             false
         }
     }
 
-    fun isPasswordChanged(): Boolean {
-        return if (passwordUser != editPassword!!.getText().toString()) {
-            reference!!.child(usernameUser!!).child("password")
-                .setValue(editPassword!!.getText().toString())
-            passwordUser = editPassword!!.getText().toString()
+    private fun isPasswordChanged(): Boolean {
+        return if (passwordUser != editPassword.text.toString()) {
+            reference.child(usernameUser!!).child("password")
+                .setValue(editPassword.text.toString())
+            passwordUser = editPassword.text.toString()
             true
         } else {
             false
         }
     }
 
-    fun showData() {
+    private fun showData() {
         val intent = intent
         nameUser = intent.getStringExtra("name")
         emailUser = intent.getStringExtra("email")
         usernameUser = intent.getStringExtra("username")
         passwordUser = intent.getStringExtra("password")
-        editName!!.setText(nameUser)
-        editEmail!!.setText(emailUser)
-        editUsername!!.setText(usernameUser)
-        editPassword!!.setText(passwordUser)
+
+        editName.setText(nameUser)
+        editEmail.setText(emailUser)
+        editUsername.setText(usernameUser)
+        editPassword.setText(passwordUser)
     }
 }
